1ab2eed22cfeedddafa8d219e66fdcc175025b6b
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2023-10-20 23:11:59 +0600

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import WheelComponent from "react-wheel-of-prizes";
 
-function App() {
+export default function App() {
+  const segments = [
+    "Ibrahim",
+    "Jashim",
+    "Jisan",
+    "Teebro",
+    "Hasibul",
+    "Rifat"
+  ];
+  const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
+
+  // Initialize the winnerName state
+  const [winnerName, setWinnerName] = useState(null);
+
+  const onFinished = (winner) => {
+    // Set the winner's name when the spin is finished
+    setWinnerName(winner);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spin Wheel(WPXPO)</h1>
+      <div>
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          onFinished={(winner) => onFinished(winner)}
+          primaryColor="black"
+          contrastColor="white"
+          buttonText="Spin It"
+          isOnlyOnce={false}
+          size={190}
+          upDuration={500}
+          downDuration={600}
+          fontFamily="Arial"
+        />
+      </div>
+      <h2  className="winner">Winner is : {winnerName}</h2> {/* Display the winner's name */}
     </div>
   );
 }
-
-export default App;
